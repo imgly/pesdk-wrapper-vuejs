9496522ef0277124741e1ad2126c3a47aceae01c
Alan Uecker <alan.uecker@img.ly>
2020-02-03 11:52:47 +0100

    Added missing files and tests

--- tests/e2e/specs/test.js
+++ tests/e2e/specs/test.js
@@ -6,16 +6,8 @@ module.exports = {
     browser
       .init()
       .waitForElementVisible('#app')
-      .assert.elementPresent('.hello')
-      .assert.containsText('h1', 'Welcome to Your Vue.js App')
-      .assert.elementCount('img', 1)
-      .end();
-  },
-
-  'example e2e test using a custom command': browser => {
-    browser
-      .openHomepage()
-      .assert.elementPresent('.hello')
+      .assert.elementPresent('.pesdk-desktop-editor')
+      .assert.elementCount('canvas', 1)
       .end();
   }
 };
